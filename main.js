6e298aa074f74b8d296084c38b3b05777285c782
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-11-17 19:54:17 -0300

    .

---
+++ main.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+
+const srv = express();
+srv.listen(3030, ()=>{
+    console.log("Aguardando conexões...");
+});
+
+srv.get('/indie', (req, res)=>{
+    const db = new sqlite3.Database(('indie.db'));
+    const sql = 'SELECT art.band, alb.album, msc.music FROM artist AS art INNER JOIN albums AS alb, musics AS msc USING(idArtist,idAlbum);';
+
+    db.all(sql, [], (erro, linhas)=>{
+        if(erro) throw erro;
+        res.json(linhas);
+    })
+});
+
+srv.get('/indie/bands', (req, res)=>{
+    const db = new sqlite3.Database(('indie.db'));
+    const sql = 'SELECT band FROM artist';
+
+    db.all(sql, [], (erro, linhas) =>{
+        if(erro) throw erro;
+        res.send(linhas);
+});
+});
+
+srv.get('/indie/albums', (req, res)=>{
+    const db = new sqlite3.Database(('indie.db'));
+    const sql = 'SELECT album FROM albums';
+
+    db.all(sql, [], (erro, linhas) =>{
+        if(erro) throw erro;
+        res.send(linhas);
+});
+});
+
+srv.get('/indie/songs', (req, res)=>{
+    const db = new sqlite3.Database(('indie.db'));
+    const sql = 'SELECT music FROM musics';
+
+    db.all(sql, [], (erro, linhas) =>{
+        if(erro) throw erro;
+        res.send(linhas);
+});
+});
+
